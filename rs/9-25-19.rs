3f6e9ddc8a6ce279360710c706fb5ae480f38e49
Samuel Wilder <pebazium@gmail.com>
2019-09-25 12:46:05 -0400

    9-25-19

--- rs/9-25-19.rs
+++ rs/9-25-19.rs
@@ -10,7 +10,7 @@ Run With:
 rustc 9-25-19.rs && 9-25-19 && rm 9-25-19.exe 9-25-19.pdb
 */
 
-fn sorted_square(mut list: Vec<i32>) -> Vec<i32> {
+fn sorted_square(mut list: Vec<i32>) -> Option<Vec<i32>> {
     let any_negatives = *list.first().unwrap() < 0;
     
     for i in list.iter_mut() {
@@ -19,14 +19,32 @@ fn sorted_square(mut list: Vec<i32>) -> Vec<i32> {
 
     // Have to sort if any of the values were negative
     if any_negatives {
+        let mut sorted = false;
         
+        while !sorted {
+            sorted = true;
+            
+            for i in 1 .. list.len() {
+                if list[i] < list[i - 1] {
+                    let tmp = list[i];
+                    list[i] = list[i - 1];
+                    list[i - 1] = tmp;
+                    sorted = false;
+                }
+            }
+        }
     }
 
-    list
+    Some(list)
 }
 
 fn main() {
     println!("Hello World!");
 
-    println!("{:?}", sorted_square(vec![-9, -2, 0, 2, 3]));
+    println!("{:?}", sorted_square(vec![-9, -2, 0, 2, 3]).unwrap());
+
+    let v = vec![
+        1, 2, 4, 8, 16, 23
+    ];
+    println!("{:?}", sorted_square(v).unwrap());
 }
