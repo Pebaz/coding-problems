ad03cd3c4d8484fdc4b6ca203a6a83dada252e59
Pebaz <pebazium@gmail.com>
2019-12-11 14:33:02 -0500

    Java compilation test

--- java/p12_11_19.java
+++ java/p12_11_19.java
@@ -1,8 +1,20 @@
+// Compile with:
+// javac p12_11_19.java && java p12_11_19 foo bar baz
 
 public class p12_11_19
 {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World!");
+
+		for (String arg : args)
+		{
+			System.out.println(arg);
+
+			for (int i = 0; i < arg.length(); i++)
+			{
+				System.out.println(arg.charAt(i));
+			}
+		}
 	}	
 }
